659a9d5673bf3b360da236c4bdb9ee1c3ed4eb50
Archziac <96952929+archziac@users.noreply.github.com>
2023-10-18 04:30:06 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function dateTime() {
 }
 
 function weatherBalloon(cityID) {
-	var apiKey = ''; //OpenWeather API key
+	var apiKey = '0aad49c894f936b25ad6663f86a790ee'; //OpenWeather API key
 	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey)
 		.then(function(resp) {
 			return resp.json()
